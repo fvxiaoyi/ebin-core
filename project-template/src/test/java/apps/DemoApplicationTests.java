8cc8b44322df19764b9a62c8660eac08388e6b6b
ebin <ebinSu@xm.remarkablefoods.com>
2022-09-05 10:58:26 +0800

    add plugin

--- project-template/src/test/java/apps/DemoApplicationTests.java
+++ project-template/src/test/java/apps/DemoApplicationTests.java
@@ -1,9 +1,14 @@
 package apps;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.HSQLDB)
 class DemoApplicationTests {
     @Test
     void contextLoads() {
