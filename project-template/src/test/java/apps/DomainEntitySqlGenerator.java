16ca71cf16afff1c1aa2bd4ae5d4657018bdb8e0
ebin <ebinSu@xm.remarkablefoods.com>
2022-11-18 11:03:58 +0800

    sql gen

--- project-template/src/test/java/apps/DomainEntitySqlGenerator.java
+++ project-template/src/test/java/apps/DomainEntitySqlGenerator.java
@@ -9,15 +9,18 @@ import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
 import org.hibernate.tool.hbm2ddl.SchemaUpdate;
 import org.hibernate.tool.hbm2ddl.UniqueConstraintSchemaUpdateStrategy;
 import org.hibernate.tool.schema.TargetType;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.orm.jpa.persistenceunit.DefaultPersistenceUnitManager;
 import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Properties;
@@ -33,12 +36,25 @@ public class DomainEntitySqlGenerator {
     }
 
     public static void stdoutUpdateSchema(String pkg, List<Class<?>> managerClasses) {
-        EntityManagerFactoryBuilderImpl builder = genEntityManagerFactoryBuilder(pkg, managerClasses);
+        EntityManagerFactoryBuilderImpl builder = genEntityManagerFactoryBuilder(managerClasses);
         builder.build();
         new SchemaUpdate().setFormat(true).execute(EnumSet.of(TargetType.STDOUT), builder.getMetadata());
     }
 
-    private static EntityManagerFactoryBuilderImpl genEntityManagerFactoryBuilder(String pkg, List<Class<?>> managerClasses) {
+    private static EntityManagerFactoryBuilderImpl genEntityManagerFactoryBuilder(List<Class<?>> managerClasses) {
+        YamlPropertiesFactoryBean factoryBean = new YamlPropertiesFactoryBean();
+        factoryBean.setResources(new ClassPathResource("application.yaml"));
+        Properties properties = factoryBean.getObject();
+        List<String> pkgs = new ArrayList<>();
+        int i = 0;
+        String pkg;
+        do {
+            pkg = (String) properties.get("spring.jpa.mysql.packagesToScan[" + i + "]");
+            if (pkg != null) {
+                pkgs.add(pkg);
+            }
+            i++;
+        } while (pkg != null);
         SpringApplication application = new SpringApplication(DataSourceAutoConfiguration.class);
         application.setWebApplicationType(WebApplicationType.NONE);
         application.setBannerMode(Banner.Mode.OFF);
@@ -49,7 +65,7 @@ public class DomainEntitySqlGenerator {
         props.put(Environment.DATASOURCE, dataSource);
         props.put(Environment.UNIQUE_CONSTRAINT_SCHEMA_UPDATE_STRATEGY, UniqueConstraintSchemaUpdateStrategy.RECREATE_QUIETLY);
         DefaultPersistenceUnitManager manager = new DefaultPersistenceUnitManager();
-        manager.setPackagesToScan(pkg);
+        manager.setPackagesToScan(pkgs.toArray(new String[]{}));
         manager.afterPropertiesSet();
         MutablePersistenceUnitInfo persistenceUnitInfo = (MutablePersistenceUnitInfo) manager.obtainDefaultPersistenceUnitInfo();
         managerClasses.forEach(clazz -> persistenceUnitInfo.addManagedClassName(clazz.getName()));
